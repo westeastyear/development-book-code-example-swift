a46409fcea032fa1a977b311c7d1acf910f77542
Donnie <seo_17@naver.com>
2022-05-07 23:50:54 +0900

    [chapter04] - (101p) 코드 작성
    Movie 타입 내부 메서드 구현

--- OBJECTS/chapter04_movieTicketSystem(Data.ver)/chapter04_movieTicketSystem(Data.ver)/Movie.swift
+++ OBJECTS/chapter04_movieTicketSystem(Data.ver)/chapter04_movieTicketSystem(Data.ver)/Movie.swift
@@ -10,10 +10,45 @@ import Foundation
 struct Movie {
     private let title: String
     private let runningTime: TimeInterval
-    private let fee: Money
-    private let discountCondition: [DiscountCondition]
+    private var fee: Money
+    private var discountCondition: [DiscountCondition]
+    private var movieType: MovieType
+    private var discountAmount: Money
+    private var discountPercent: Double
     
-    private let movieType: MovieType
-    private let discountAmount: Money
-    private let discountPercent: Double
+    func getMovieType() -> MovieType {
+        return self.movieType
+    }
+    
+    func getDiscountPercent() -> Double {
+        return self.discountPercent
+    }
+    
+    mutating func setMovieType(movieType: MovieType) {
+        self.movieType = movieType
+    }
+    
+    mutating func setFee(fee: Money) {
+        self.fee = fee
+    }
+    
+    mutating func setDiscountCondition(discountCondition: [DiscountCondition]) {
+        self.discountCondition = discountCondition
+    }
+    
+    mutating func getDiscountAmount() -> Money {
+        return discountAmount
+    }
+    
+    mutating func setDiscountAmount(discountAmount: Money) {
+        self.discountAmount = discountAmount
+    }
+    
+    mutating func setDiscountPercent(discountPercent: Double) {
+        self.discountPercent = discountPercent
+    }
+    
+//    public List<DiscountCondition> getDiscountCondition() {
+//        return Collections.unmodifiableList(discountConditions);
+//    }
 }
